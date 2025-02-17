51d1469183d392b4500e49693898727ab622d7f9
KMartin207 <martinkothencz@gmail.com>
2025-02-17 10:34:22 +0100

    Add files via upload

---
+++ szorzotabla/szorzotabla/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szorzotabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int valasz = kiiras();
+
+            if(valasz == 1)
+            {
+                feladat();
+            }
+            else if(valasz == 2)
+            {
+                rangsor();
+            }
+
+            Console.ReadKey();
+
+        }
+
+        private static void rangsor()
+        {
+            
+        }
+
+        private static void feladat()
+        {
+            string nev = felhasz();
+
+
+            
+
+        }
+
+        private static string felhasz()
+        {
+            Console.WriteLine("Feladatsor");
+            Console.WriteLine("Kérlek írj be egy felhasználónevet: ");
+            string felhasznalo = Console.ReadLine();
+
+            return felhasznalo;
+
+        }
+
+        private static int kiiras()
+        {
+            Console.WriteLine("Szorzótábala");
+
+            Console.WriteLine("Menü");
+            Console.WriteLine("\t1.) Feladatsor");
+            Console.WriteLine("\t2.) Ranglista");
+
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+
+            return szam;
+        }
+    }
+}
