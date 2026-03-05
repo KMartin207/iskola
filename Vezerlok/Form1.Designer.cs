55857eed422e0c8abf8b46eb6a4312e0ffbd1764
KMartin207 <martinkothencz@gmail.com>
2026-03-05 09:41:19 +0100

    Add files via upload

---
+++ Vezerlok/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Vezerlok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VezerloGrp = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.VezerloGrp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // VezerloGrp
+            // 
+            this.VezerloGrp.Controls.Add(this.hScrollBar3);
+            this.VezerloGrp.Controls.Add(this.panel1);
+            this.VezerloGrp.Controls.Add(this.hScrollBar2);
+            this.VezerloGrp.Controls.Add(this.hScrollBar1);
+            this.VezerloGrp.Controls.Add(this.label3);
+            this.VezerloGrp.Controls.Add(this.label2);
+            this.VezerloGrp.Controls.Add(this.label1);
+            this.VezerloGrp.Location = new System.Drawing.Point(12, 12);
+            this.VezerloGrp.Name = "VezerloGrp";
+            this.VezerloGrp.Size = new System.Drawing.Size(776, 426);
+            this.VezerloGrp.TabIndex = 0;
+            this.VezerloGrp.TabStop = false;
+            this.VezerloGrp.Text = "Vezérlők";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Blue";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(282, 96);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(80, 17);
+            this.hScrollBar1.TabIndex = 3;
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(282, 157);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(80, 17);
+            this.hScrollBar2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(506, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(203, 157);
+            this.panel1.TabIndex = 6;
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(282, 232);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(80, 17);
+            this.hScrollBar3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.VezerloGrp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.VezerloGrp.ResumeLayout(false);
+            this.VezerloGrp.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox VezerloGrp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+    }
+}
+
