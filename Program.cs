83279619a72b97d07ad35e8cdede27208e783361
KMartin207 <martinkothencz@gmail.com>
2026-03-29 21:05:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvOsztaly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            konyv konyv1 = new konyv();
+
+            konyv1("nsjdn", "dfdsf", 23);
+
+
+            
+
+        }
+    }
+}
---
+++ konyv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace konyvOsztaly
+{
+    public class konyv
+    {
+        public string szerzo;
+        public string cim;
+        public int oldal;
+
+
+        public konyv(string szerzoje, string cime, int oldala)
+        {
+            this.szerzo = szerzoje;
+            this.cim = cime;
+            this.oldal = oldala;
+        }
+
+
+    }
+}
